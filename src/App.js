e3d64d148de5c3fb093b7cc8f7b841258607d4de
iamgique <iamgique@gmail.com>
2021-01-26 23:17:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase/app';
+
+class App extends Component {
+  constructor(props){
+      super(props);
+      const config = {
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID
+      };
+      if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+      }
+  }
+  render() {
+    return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              {process.env.REACT_APP_PROJECT_ID}
+            </p>
+          </header>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
